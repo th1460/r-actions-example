d93251497d0b0288ce227beab1db9b52cbca1133
Thiago-Pires <Thiago.Pires@ibm.com>
2021-02-25 13:47:48 -0300

    first commit

---
+++ script.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+# get input
+args <- commandArgs(trailingOnly=TRUE)
+
+# function
+A <- function(x) x^2
+
+# input
+x <- as.numeric(args[1])
+
+# output
+cat(jsonlite::toJSON(list(side = x, area = A(x)), auto_unbox = TRUE)) 
